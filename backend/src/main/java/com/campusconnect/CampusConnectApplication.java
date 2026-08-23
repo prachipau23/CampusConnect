d6e3e109d66ef3885e915a8b92ea92be0501538f
prachipau23 <prachipau@gmail.com>
2026-08-23 21:27:35 +0530

    feat: main application class

--- backend/src/main/java/com/campusconnect/CampusConnectApplication.java
+++ backend/src/main/java/com/campusconnect/CampusConnectApplication.java
@@ -2,17 +2,9 @@ package com.campusconnect;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CampusConnectApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(CampusConnectApplication.class);
-    }
-
+public class CampusConnectApplication {
     public static void main(String[] args) {
         SpringApplication.run(CampusConnectApplication.class, args);
     }
